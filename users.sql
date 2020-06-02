34960a1132bc0488d46e5efe39ea4e4568ad4b40
Heiner <khh8615@nwestnetwork.com>
2020-06-02 16:10:04 -0700

    updates

--- users.sql
+++ users.sql
@@ -6,31 +6,25 @@ col INITIAL_RSRC_CONSUMER_GROUP for A30
 col default_tablespace for A20
 select username, created, profile, account_status, default_tablespace, INITIAL_RSRC_CONSUMER_GROUP
 from dba_users 
-where regexp_like (username,'^([[:alpha:]]{2}[[:digit:]]{4})$')
-or regexp_like (username,'^([[:alpha:]]{2}[[:digit:]]{3}[[:alpha:]])$')
+where regexp_like (username,'^([[:alpha:]]{3}[[:digit:]]{4})$')
+or regexp_like (username,'^([[:alpha:]]{3}[[:digit:]]{3})$')
+or regexp_like (username,'^V_([[:alpha:]]{3}[[:digit:]]{3})$')
 order by 1
 /
+
 
 set pages 200 lines 250
 col username for A25
 select username, created, profile, account_status, default_tablespace, INITIAL_RSRC_CONSUMER_GROUP
 from dba_users 
-where username  between 'M00000' and 'M99999'
-order by 1
-/
-
-set pages 200 lines 250
-col username for A25
-select username, created, profile, account_status, default_tablespace, INITIAL_RSRC_CONSUMER_GROUP
-from dba_users 
-where not (regexp_like (username,'^([[:alpha:]]{2}[[:digit:]]{4})$')
-or regexp_like (username,'^([[:alpha:]]{2}[[:digit:]]{3}[[:alpha:]])$'))
-and nvl(password,'x') != 'EXTERNAL'
-and username not between 'M00000' and 'M99999'
+where not regexp_like (username,'^([[:alpha:]]{3}[[:digit:]]{4})$')
+and not regexp_like (username,'^([[:alpha:]]{3}[[:digit:]]{3})$')
+and not regexp_like (username,'^V_([[:alpha:]]{3}[[:digit:]]{3})$')
+--and nvl(password,'x') != 'EXTERNAL'
+--and username not between 'M00000' and 'M99999'
 and username not in (
 'ANONYMOUS'
 ,'APPQOSSYS'
-,'ATT_SHAREDO'
 ,'DBA_DBG'
 ,'DBA_MON'
 ,'DBSNMP'
